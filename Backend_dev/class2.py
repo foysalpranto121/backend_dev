4c507d1c920df72ff11a8a306e06d7f82f6bf43a
foysalpranto121 <foysalpranto2002@gmail.com>
2026-06-29 18:02:37 +0600

    Add files

---
+++ Backend_dev/class2.py
@@ -0,0 +1,19 @@
+
+price = 100
+price1,price2,price3 = 10,20,30
+price5 = price6 = price7 = 100
+print(price1,price2,price3)
+print(price5,price6,price7)
+
+quantity = 2
+shirt = 100*quantity
+pant = 700*quantity
+total_price = shirt+pant
+
+print(total_price)
+
+pie = 3.1416
+r = 30
+r_square = r**2
+pie_squre = pie*r_square
+print(pie_squre)
