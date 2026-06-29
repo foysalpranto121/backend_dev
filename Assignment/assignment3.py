8e01e357a6e9c4234fa2d0831827d0dc857838ec
foysalpranto121 <foysalpranto2002@gmail.com>
2026-06-29 18:42:55 +0600

    added assignmnet 3

---
+++ Assignment/assignment3.py
@@ -0,0 +1,10 @@
+Checkup_Fee =1000
+x_ray_Fee = 5000
+Filling_Fee = 2000
+total_Fee = Checkup_Fee + x_ray_Fee + Filling_Fee
+print("Total fee :", total_Fee)
+vat=0.10
+final_bill=total_Fee+(total_Fee*vat)
+print("Final bill with vat:", final_bill)
+
+
