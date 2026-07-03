a5172bd0b1fbebadab7718cd247a41949218cd87
foysalpranto121 <foysalpranto2002@gmail.com>
2026-07-03 10:20:57 +0600

    added 3rd class

--- Class materials/class3.py
+++ Class materials/class3.py
@@ -5,4 +5,4 @@ b=float(input("Enter b: "))
 c=float(input("Enter c: "))
 d=float(input("Enter d: "))
 e=float(input("Enter e: "))
-ans =(((a+b)**2-(math.sqrt(d))*c)-4+((e)**2-a*c)+math.sqrt(b-1)-((a-c)*(b+2)+math.sqrt(a*a+2))/2)**3
+ans=(((a+b)**2-(math.sqrt(d))*c)-4+((e)**2-a*c)+math.sqrt(b-1)-((a-c)*(b+2)+math.sqrt(a*a+2))/2)**3
