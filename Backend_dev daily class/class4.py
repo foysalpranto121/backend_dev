9cf8dd4cab82e4facb9e77cd5761683076189e4a
foysalpranto121 <foysalpranto2002@gmail.com>
2026-07-14 10:59:09 +0600

    added new file

---
+++ Backend_dev daily class/class4.py
@@ -0,0 +1,22 @@
+## list and for loop 
+list1=["kasem","hasem","rahim","jhon","ahmed"]
+print(list1)
+## list changeabke--->> 
+# 1- add
+# 2-remove 
+# 3- duplicate allowed 
+
+
+list2=["kasem","hasem","rahim","jhon","ahmed","kasem"]
+print(list2) ## allowed duplicated 
+## len() func 
+print(len(list2))
+
+print(type(list2))
+
+## index 
+print(list2[2])
+print(list2[-1])
+print(list2[2:4])
+print(list2[:4])
+print(list2[2:])
