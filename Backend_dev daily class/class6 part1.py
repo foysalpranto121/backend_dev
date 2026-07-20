bbfe030d2733798020cef7f17976be253e3a90e6
foysalpranto121 <foysalpranto2002@gmail.com>
2026-07-20 11:19:03 +0600

    added new file

---
+++ Backend_dev daily class/class6 part1.py
@@ -0,0 +1,5 @@
+## list one dimensional=0ne row  and multiple column
+product_list=['chinri mas','potol','khatal','chal','dhal']
+product_price=[1000,20,300,100,150]
+quantity=[1,3,3,2,3]
+print(product_list[4])
