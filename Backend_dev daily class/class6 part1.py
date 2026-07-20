fe1010eb9221261052e0c6d03ed7d743c86423db
foysalpranto121 <foysalpranto2002@gmail.com>
2026-07-20 13:15:54 +0600

    added new file

--- Backend_dev daily class/class6 part1.py
+++ Backend_dev daily class/class6 part1.py
@@ -3,3 +3,14 @@ product_list=['chinri mas','potol','khatal','chal','dhal']
 product_price=[1000,20,300,100,150]
 quantity=[1,3,3,2,3]
 print(product_list[4])
+print(len(product_list))
+## ploymorphism=same entity different behaviour
+## len is a function based on polymorphism
+product_list.append('milk')
+product_price.append(80)
+quantity.append(2)
+print(product_list)
+size=len(product_list)
+print(size)
+for i in range(size):
+    print(product_list[i],product_price[i],quantity[i],product_price[i]*quantity[i])
