c25b9f2573232baaa2e06e4c3c87583b8dc3fd62
foysalpranto121 <foysalpranto2002@gmail.com>
2026-07-22 01:15:04 +0600

    added new file

---
+++ Backend_dev daily class/class8 part2.py
@@ -0,0 +1,13 @@
+import cv2
+img = cv2.imread(r"C:\Users\moonp\OneDrive\Pictures\pranto.jpg")
+if img is None:
+    print("Error: Could not load image. Check if 'pranto.jpg' exists in the current directory.")
+else:
+    cv2.imshow("My picture", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    ## cv2.IMREAD_GRAYSCALE flag is converting the image to black and white 
+    print(img.shape)
+    print(len(img[0]))
+
