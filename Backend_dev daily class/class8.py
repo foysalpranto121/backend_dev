d6675dc25edbba4ac2e3d1dd925d2a4e186ce859
foysalpranto121 <foysalpranto2002@gmail.com>
2026-07-21 21:16:51 +0600

    added new file

---
+++ Backend_dev daily class/class8.py
@@ -0,0 +1,12 @@
+## 2D list 
+book_self=[
+    ["book1","book2","book3"],
+    ["book4","book5","book6"],
+    ["book7","book8","book9"]
+]
+total_row=len(book_self)
+total_column=len(book_self[0])
+print(book_self)
+print(total_row)
+print(total_column)
+print(book_self[1][1])
